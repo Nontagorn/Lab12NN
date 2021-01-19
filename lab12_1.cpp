aef7cd0454960abdcfa907b4b3f79a56452d810b
PondD <pangpon_2542@hotmail.com>
2021-01-19 20:06:08 +0700

    lab12

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,23 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N, double B[]){
+    double sum = 0, min = A[0], max = A[0], sumstd = 0, mul = 1, sumha;
+    for(int i = 0; i < N; i++){
+        sum += A[i];
+        mul *= A[i];
+        sumha += 1.0/A[i];
+        if(A[i] > max) max = A[i];
+        if(A[i] < min) min = A[i];
+    }
+    B[0] = sum/N;
+    for(int i = 0; i < N; i++){
+        sumstd += (pow(A[i],2)) - pow(B[0],2);
+    }
+    B[1] = sqrt(1.0/N*sumstd);
+    B[2] = pow(mul,1.0/N);
+    B[3] = N/sumha;
+    B[4] = max;
+    B[5] = min;
+}
